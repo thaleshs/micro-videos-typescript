0cc564625a2b46504a0f4cec68995aae3484245e
Thales Saraiva <th2s@live.com>
2022-09-17 17:59:28 +0000

    refactor: private property
    change id to a private property

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -3,7 +3,7 @@ import InvalidUuidError from "../../errors/invalid-uuid.error";
 import ValueObject from "./value-object";
 
 export default class UniqueEntityId extends ValueObject<string> {
-  constructor(readonly id?: string) {
+  constructor(private readonly id?: string) {
     super(id || uuidv4());
     this.validate();
   }
